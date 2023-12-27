90bb83eb106041badb818d55a206743a51795fe2
M-Burak-Yilmazer <mbyilmazer38@gmail.com>
2023-12-27 18:40:58 +0300

    import tostify

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
 
 import NavbarPart from "./components/NavbarPart";
-
+import "react-toastify/dist/ReactToastify.css";
 import Router from "./router/Router";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div>
       <NavbarPart />
-
       <Router />
+      <ToastContainer />
     </div>
   );
 }
